c0c791036cf4581910ea2cf4f0fa69d81599fe71
FelixSelter <felixselterdeveloping@gmail.com>
2022-06-02 11:43:00 +0200

    code is working

--- Analysis.py
+++ Analysis.py
@@ -34,8 +34,6 @@ def analyse1():
         g.add_edge(tags[0], tags[1], weight=importance * 100)
         edges += 1
 
-    print(edges)
-
     fig = plt.figure(1, figsize=(50, 50), dpi=100)
     pos = nx.spring_layout(g, k=2, iterations=50)
     nx.draw(g, with_labels=True, node_size=[amount * 500 for amount in tag_counter.values()],
@@ -75,8 +73,9 @@ def analyse3():
               WHERE wiki = false)
         GROUP BY author
         ORDER BY contributions DESC
+        LIMIT 10
     """)
-    author, amount = zip(*(cursor.fetchall()[:10]))
+    author, amount = zip(*(cursor.fetchall()))
 
     fig = plt.figure()
     fig.suptitle('Questions and answers', fontsize=20)
@@ -96,8 +95,9 @@ def analyse4():
         WHERE wiki = false
         GROUP BY author
         ORDER BY questions DESC
+        LIMIT 10
     """)
-    author, amount = zip(*(cursor.fetchall()[:10]))
+    author, amount = zip(*(cursor.fetchall()))
 
     fig = plt.figure()
     fig.suptitle('Questions', fontsize=20)
@@ -117,8 +117,9 @@ def analyse5():
         WHERE wiki = false
         GROUP BY author
         ORDER BY answers DESC
+        LIMIT 10
     """)
-    author, amount = zip(*(cursor.fetchall()[:10]))
+    author, amount = zip(*(cursor.fetchall()))
 
     fig = plt.figure()
     fig.suptitle('Answers', fontsize=20)
@@ -130,4 +131,111 @@ def analyse5():
     plt.savefig('answers.png', dpi=100)
 
 
-analyse5()
+def analyse6():
+    global cursor
+    cursor.execute("""
+        SELECT author, score
+        FROM (SELECT questions.author, questions.score
+              FROM questions
+              UNION ALL
+              SELECT answers.author, answers.score
+              FROM answers)
+        GROUP BY author
+        ORDER BY score ASC
+        LIMIT 10
+    """)
+    author, score = zip(*(cursor.fetchall()))
+
+    fig = plt.figure()
+    fig.suptitle('Least score', fontsize=20)
+    plt.xticks(rotation=90)
+    plt.xlabel('Username', fontsize=16)
+    plt.ylabel('Score', fontsize=16)
+    plt.bar(author, score)
+    plt.tight_layout()
+    plt.savefig('least score.png', dpi=100)
+
+
+def analyse7():
+    global cursor
+    cursor.execute("""
+        SELECT author, score
+        FROM (SELECT questions.author, questions.score
+              FROM questions
+              UNION ALL
+              SELECT answers.author, answers.score
+              FROM answers)
+        GROUP BY author
+        ORDER BY score DESC
+        LIMIT 10
+    """)
+    author, score = zip(*(cursor.fetchall()))
+
+    fig = plt.figure()
+    fig.suptitle('Most score', fontsize=20)
+    plt.xticks(rotation=90)
+    plt.xlabel('Username', fontsize=16)
+    plt.ylabel('Score', fontsize=16)
+    plt.bar(author, score)
+    plt.tight_layout()
+    plt.savefig('most score.png', dpi=100)
+
+
+def analyse8():
+    global cursor
+    cursor.execute("""
+        SELECT Count(score)
+        FROM (SELECT questions.score
+              FROM questions
+              UNION ALL
+              SELECT  answers.score
+              FROM answers)
+        WHERE score = 0
+        GROUP BY score
+
+    """)
+    zero = cursor.fetchone()[0]
+
+    cursor.execute("""
+            SELECT Count(score)
+            FROM (SELECT questions.score
+                  FROM questions
+                  UNION ALL
+                  SELECT  answers.score
+                  FROM answers)
+            WHERE score < 0
+            GROUP BY score
+
+        """)
+    less = cursor.fetchone()[0]
+
+    cursor.execute("""
+            SELECT Count(score)
+            FROM (SELECT questions.score
+                  FROM questions
+                  UNION ALL
+                  SELECT  answers.score
+                  FROM answers)
+            WHERE score > 0
+            GROUP BY score
+
+        """)
+    more = cursor.fetchone()[0]
+
+    fig, ax = plt.subplots()
+    fig.suptitle('Score less/more/zero', fontsize=20)
+    ax.pie([zero, less, more], labels=["Zero", "Negative", "Positive"], autopct='%1.11f%%', startangle=90)
+    ax.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+    plt.tight_layout()
+    plt.savefig('score+-.png', dpi=100)
+
+
+if __name__ == "__main__":
+    # analyse8()
+    analyse1()
+# analyse2()
+# analyse3()
+# analyse4()
+# analyse5()
+# analyse6()
+# analyse7()
